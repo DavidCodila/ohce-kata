bd8320605d3dd56e7eba43258aac18a5100f3570
david <Davids-MacBook-Pro.local>
2024-04-19 09:10:14 +1000

    Added failing Palindrome test

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "bun:test";
-import { ohce } from "../src/index";
+import { ohce, ohceNextLine } from "../src/index";
 
 test("Greeting at hour 21", () => {
   expect(ohce(21, "david")).toEqual("¡Buenas noches david!");
@@ -14,5 +14,9 @@ test("Greeting at hour 6", () => {
 });
 
 test("Greeting at hour 12", () => {
-  expect(ohce(7, "david")).toEqual("¡Buenos tardes david!");
+  expect(ohce(12, "david")).toEqual("¡Buenas tardes david!");
+});
+
+test("Palindrome test", () => {
+  expect(ohceNextLine("abba")).toEqual("abba\n¡Bonita palabra!");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 export function ohce(time: number, name: string): string {
   if (time > 19 || time < 6) return "¡Buenas noches david!";
-  else return "¡Buenos días david!";
+  else if (time > 11 && time < 20) {
+    return "¡Buenas tardes david!";
+  } else return "¡Buenos días david!";
+}
+
+export function ohceNextLine(word: string): string {
+  return "";
 }
