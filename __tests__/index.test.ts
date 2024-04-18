ac551ba34941ba0039ef584bff3da8b2ac1035ed
david <Davids-MacBook-Pro.local>
2024-04-18 16:22:53 +1000

    Wrote failing Greeting between 6 and 12 hours test

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -4,3 +4,7 @@ import { ohce } from "../src/index";
 test("Greeting between 20 and 6 hours", () => {
   expect(ohce(21, "david")).toEqual("¡Buenas noches david!");
 });
+
+test("Greeting between 6 and 12 hours", () => {
+  expect(ohce(21, "david")).toEqual("¡Buenos días david!");
+});
