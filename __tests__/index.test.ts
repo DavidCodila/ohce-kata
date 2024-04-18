bee4aa1ab4c71210b73c13ceba9ec13d898fd433
david <Davids-MacBook-Pro.local>
2024-04-18 16:31:46 +1000

    Added failing test case of the 20 to 6 hour greeting

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -5,6 +5,10 @@ test("Greeting between 20 and 6 hours", () => {
   expect(ohce(21, "david")).toEqual("¡Buenas noches david!");
 });
 
+test("Greeting between 20 and 6 hours", () => {
+  expect(ohce(20, "david")).toEqual("¡Buenas noches david!");
+});
+
 test("Greeting between 6 and 12 hours", () => {
   expect(ohce(6, "david")).toEqual("¡Buenos días david!");
 });
