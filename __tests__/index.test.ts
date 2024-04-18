d9f0fee503ee104807c9c7cc4f6dbdbe67eaa483
david <Davids-MacBook-Pro.local>
2024-04-18 16:41:23 +1000

    Passed Greeting at hour 6 test

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,14 +1,14 @@
 import { expect, test } from "bun:test";
 import { ohce } from "../src/index";
 
-test("Greeting between 20 and 6 hours", () => {
+test("Greeting at hour 21", () => {
   expect(ohce(21, "david")).toEqual("¡Buenas noches david!");
 });
 
-test("Greeting between 20 and 6 hours", () => {
+test("Greeting at hour 20", () => {
   expect(ohce(20, "david")).toEqual("¡Buenas noches david!");
 });
 
-test("Greeting between 6 and 12 hours", () => {
-  expect(ohce(6, "david")).toEqual("¡Buenos días david!");
+test("Greeting at hour 6", () => {
+  expect(ohce(7, "david")).toEqual("¡Buenos días david!");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export function ohce(time: number, name: string): string {
-  if (time == 21) return "¡Buenas noches david!";
+  if (time > 19 || time < 6) return "¡Buenas noches david!";
   else return "¡Buenos días david!";
 }
