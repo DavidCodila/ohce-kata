ec6cc31381759662e4afacbf5432f81a7525e320
david <Davids-MacBook-Pro.local>
2024-04-19 10:25:23 +1000

    Added readline functionality

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
+import * as readline from "node:readline/promises";
+
+import { stdin as input, stdout as output } from "node:process";
+
+const rl = readline.createInterface({ input, output });
+
+const answer = await rl.question("Write something: ");
+
+console.log("You wrote: " + answer);
+
+rl.close();
+
 export function ohce(time: number, name: string): string {
   if (time > 19 || time < 6) return "¡Buenas noches david!";
   else if (time > 11 && time < 20) {
